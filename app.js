36a47d9fe22bf268b69c75f3953ac1753bfd57fb
Kit Ewbank <Kit.Ewbank@capitalone.com>
2015-07-09 13:58:44 -0400

    Display transactions.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@
  * Entry point.
  */
 
+// npm start -- --address 1DJByF3cKrRJwwztNocjpyWmSeV4wFCvEs
+
 var util = require('util');
 var Rx = require('rx');
 var Chain = require('chain-node');
@@ -32,6 +34,17 @@ var observer = {
     },
 };
 
+// Parse command line.
+var program = require('commander');
+program.
+    version(require('./package.json').version).
+    option('-a, --address [bitcoin address]', 'Bitcoin address').
+    parse(process.argv);
+if (!program.address) {
+    console.log('No bitcoin address specified');
+    process.exit(1);
+}
+
 // Read in configuration.
 var publicConfig = require('./config/public.config.json'),
     privateConfig = require('./config/private.config.json');
@@ -42,6 +55,16 @@ var chain = new Chain({
     blockChain: publicConfig.chain.blockchain
 });
 
-Rx.Observable.fromNodeCallback(chain.getAddress, chain)('17x23dNjXJLzGMev6R63uyRhMWP1VHawKc').
+Rx.Observable.fromNodeCallback(chain.getAddress, chain)(program.address).
     merge(sigint).
+    flatMap(function (addresses) {
+        return Rx.Observable.from(addresses).first();
+    }).
+    flatMap(function (address) {
+        util.log('Basic details: ' + util.inspect(address));
+        return Rx.Observable.fromNodeCallback(chain.getAddressTransactions, chain)(address.address);
+    }).
+    flatMap(function (transactions) {
+        return Rx.Observable.from(transactions);
+    }).
     subscribe(observer);
